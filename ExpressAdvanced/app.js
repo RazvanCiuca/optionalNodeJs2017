d5ab5edf8eb4a5ce345ed11bfda6bbc635730ec9
Razvan Ciuca <razvann88@gmail.com>
2017-10-26 10:34:58 +0300

    add express advanced course

---
+++ ExpressAdvanced/app.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.set('views', './src/views');
+// app.set('view engine', 'ejs');
+
+const port = 3000;
+
+const sections = [
+    {
+        title: 'Featured',
+        items: [
+            {
+                id: 1,
+                title: 'Trump bans romanians',
+                source: 'romaniatv.ro',
+                timestamp: '1 hour ago'
+            }
+        ]
+    },
+    {
+        title: 'Wishlist',
+        items: [
+            {
+                id: 2,
+                title: 'Dragnea condamnat a doua oara',
+                source: 'antena3.tv',
+                timestamp: '2 hour ago'
+            },
+            {
+                id: 3,
+                title: 'Jumatate din parlamentul Romaniei gasit rastignit in Piata Constitutiei',
+                source: 'digi24.ro',
+                timestamp: '4 hour ago'
+            }
+        ]
+    }
+];
+
+const articles = [
+    {
+        id: 1,
+        title: 'Trump bans romanians',
+        content: "One a those days, huh. Wal, a wiser fella than m'self once said, sometimes you eat the bar and sometimes the bar, wal, he eats you. That wasn't her toe. Excuse me! Mark it zero. Next frame. Mind if I smoke a jay? Brandt can't watch though. Or he has to pay a hundred. Sex. The physical act of love. Coitus. Do you like it? Shomer shabbos."
+    },
+    {
+        id: 2,
+        title: 'Dragnea condamnat a doua oara',
+        content: "Tomorrow vee come back und cut off your chonson. Walter, you can't do that. These guys're like me, they're pacifists. Smokey was a conscientious objector. You got the wrong guy. I'm the Dude, man. I'm not Mr. Lebowski; you're Mr. Lebowski. I'm the Dude. I got a nice quiet beach community here, and I aim to keep it nice and quiet. Vee belief in nossing. Okay. Vee take ze money you haf on you und vee call it eefen."
+    },
+    {
+        id: 3,
+        title: 'Jumatate din parlamentul Romaniei gasit rastignit in Piata Constitutiei',
+        content: "The Knutsens. It's a wandering daughter job. Bunny Lebowski, man. Her real name is Fawn Knutsen. Her parents want her back. I hope you're not avoiding this call because of the rug, which, I assure you, is not a problem. I know how he likes to present himself; Father's weakness is vanity. Hence the slut. Ja, it seems you forgot our little deal, Lebowski. DO YOU SEE WHAT HAPPENS, LARRY?"
+    }
+];
+
+const router = express.Router();
+const articleRouter = express.Router();
+
+
+router.get('/', function(req, res) {
+    res.render('index', {sections: sections});
+});
+
+articleRouter.route('/:id')
+    .get(function(req, res) {
+        const article = _.find(articles, {id: parseInt(req.params.id, 10)});
+        res.render('article', {article: article});
+    });
+
+
+app.use('/', router);
+app.use('/articles', articleRouter);
+
+app.listen(port, function() {
+    console.log('Listening on ', port);
+});
